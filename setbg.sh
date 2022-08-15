ec82596f0013638ada1e97c9e7248efebab5962c
tokyo <42143007+2b2tplayer@users.noreply.github.com>
2022-08-15 18:29:55 -0300

    Update setbg.sh

--- setbg.sh
+++ setbg.sh
@@ -3,13 +3,10 @@
 echo "choosing wallpaper"
 wall=$(find $HOME/pix/wall -type f | shuf -n 1)
 echo "setting up wallpaper"
-xwallpaper --zoom $wall
 echo "generating color scheme"
 wal -i $wall >/dev/null
 sed -i 'N;$!P;D' $HOME/.cache/wal/colors-wal-dwm.h
 echo "recompiling dwm"
 (cd $HOME/.config/dwm && sudo make clean install>/dev/null)
-
-xwallpaper --zoom $wall
-
-echo "restart dwm for new look"
+echo "restarting dwm for new look"
+xdotool key super+shift+q
